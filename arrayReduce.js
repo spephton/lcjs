9205209844eb7b2d977bc3c6035c176c95cb057b
Jacob Sephton <71482036+spephton@users.noreply.github.com>
2023-05-14 17:08:00 +1000

    Index a list of objects by id

--- arrayReduce.js
+++ arrayReduce.js
@@ -15,3 +15,18 @@ const sum = (acc, x) => {
 let arr = [1, 2, 3, 4, 5];
 
 console.log(reduce(arr, sum, 0));
+
+
+// array indexing is a sick concept I want to practice implementing that
+const groceries = [
+    { id: 171, name: "ham" },
+    { id: 12, name: "cheese" }, 
+    { id: 535, name: "croissant" },
+]
+
+const indexedGroceries = groceries.reduce((acc, val) => {
+    acc[val.id] = val;
+    return acc; // using spread syntax here would make perf suck
+}, {});
+
+console.log(indexedGroceries);
