0ac244842c2ece16f4bc0141c6489a7a55d369e8
Jacob Sephton <71482036+spephton@users.noreply.github.com>
2023-05-22 00:20:31 +1000

    Implement throttle()

--- throttle.mjs
+++ throttle.mjs
@@ -1,29 +1,88 @@
-const throttle = function(fn, t) {
+const returningThrottle = function(fn, t) {
     let args;
     let curPromise;
     let promiseActive;
     let curArgs;
+    let shouldCall;
     return function(...args) {
         curArgs = [...args];
         if (!promiseActive) {
+            shouldCall = false;
             promiseActive = true;
             curPromise = new Promise((res) => {
                 setTimeout(() => {
                     promiseActive = false;
+
                     res(fn(...curArgs));
                 }, t);
             });
+            return fn(...args);
         }
         return curPromise;
     }
 }
 
-const add = (a, b) => a + b;
-const thradd = throttle(add, 50);
-
-let timeout = thradd(1, 2);
-timeout = thradd(1, 1);
-
-console.log(await timeout);
 
 let sleep = async (t) => new Promise((res) => setTimeout(res, t));
+
+// let's do this again but simpler
+// don't bother trying to resolve a promise until you have the pattern down 
+const throttle = (fn, t) => {
+    let activeTimeout = false;
+    let shouldResolve = false;
+    let currentArgs;
+    return (...args) => {
+        currentArgs = [...args];
+
+        function makeNewTimeout() {
+            activeTimeout = true;
+            return setTimeout(() => {
+                if (shouldResolve) {
+                    shouldResolve = false;
+                    makeNewTimeout();
+                    fn(...currentArgs);
+                }
+                else {
+                    // expire harmlessly
+                    activeTimeout = false;
+                }
+            }, t);
+        }
+
+        if (!activeTimeout) {
+            makeNewTimeout();
+            return fn(...args);
+        }
+        else {
+            // we've already updated args
+            // make sure function resolves when timeout expires
+            shouldResolve = true
+        }
+    }
+}
+
+function syncSleep(t) {
+    const now = Date.now();
+    while (Date.now() < now + t) {}
+    return;
+}
+
+const add = (a, b) => console.log(a + b);
+const thradd = throttle(add, 1000);
+
+thradd(1, 2);
+await sleep(1);
+thradd(1, 1);
+
+thradd(7, 6);
+thradd(1, 5);
+await sleep(700);
+thradd(1, 3);
+await sleep (20);
+thradd(12, 40);
+await sleep(400);
+thradd(60, 9);
+await sleep(1);
+thradd(5, 5);
+await sleep(1);
+thradd(400, 20);
