bf4717407fcf259d3b3d898e087cf83daf289834
Jacob Sephton <71482036+spephton@users.noreply.github.com>
2023-05-22 22:50:04 +1000

    Not really any faster deep equals lol

--- jsonDeepEqual.mjs
+++ jsonDeepEqual.mjs
@@ -5,28 +5,30 @@ console.log(object);
 let object2 = JSON.parse(await fs.readFile('bugbjk2.json', {encoding: 'utf-8'}));
 console.log(object2);
 
-const isObject = (x) => (x !== null && typeof(x) === "object");
 
 let areDeeplyEqual = function(o1, o2) {
 	let equals;
-	if (o1 === null) {
-		return (o2 === null);
-	}
-	// not an object or array
-	if (typeof(o1) !== "object") {
-		return (o1 === o2);
-	}
-	if (o1.constructor !== o2.constructor) return false;
-	if (Object.keys(o1).length !== Object.keys(o2).length) return false;
-	for (const [key, value] of Object.entries(o1)) {
-		//objects (and arrays!)
-		if (isObject(value)) {
-			if (!isObject(o2[key])) return false; // check also object
-			equals = areDeeplyEqual(o1[key], o2[key]); // compare props
+	if (o1 === o2) return true;
+	if (o1 === null || o1 === undefined || o2 === null || o2 === undefined) return false;
+	if (typeof(o1) !== "object") return false; // if it's not equal and not an object/array it's unequal
+
+	
+	if (o1.constructor === Array) {
+		if (o2.constructor !== Array || o1.length !== o2.length) return false;
+		for (let i = 0; i < o1.length; i++) {
+			if (o1[i] === o2[i]) continue;
+			equals = areDeeplyEqual(o1[i], o2[i]);
 			if (!equals) return false;
 		}
-		else {
-			if (value !== o2[key]) return false;
+	}
+	else if (o1.constructor === Object) {
+		if (o2.constructor !== Object) return false;
+		if (Object.keys(o1).length !== Object.keys(o2).length) return false;
+		
+		for (const [key, value] of Object.entries(o1)) {
+			if (value === o2[key]) continue;
+			equals = areDeeplyEqual(value, o2[key]); // compare props
+			if (!equals) return false;
 		}
 	}
 	return true;
