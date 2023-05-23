7a541763951358549f4f620c7dc3afab731351e1
Jacob Sephton <71482036+spephton@users.noreply.github.com>
2023-05-23 10:45:32 +1000

    Implement JSON.Stringify

---
+++ jsonStringify.mjs
@@ -0,0 +1,57 @@
+import fs from 'fs/promises';
+
+let jsonStringify = function(object) {
+	let thisObject = '';
+	if (typeof(object) === 'string') {
+		return '"' + String(object) + '"';
+	}
+	if (object == null || typeof(object) === 'number' || typeof(object) === 'boolean') {
+		return String(object);
+	}
+	else if (typeof(object) === 'object') {
+		if (Array.isArray(object)) {
+			thisObject += '[';
+			for (let i = 0; i < object.length; i++) {
+				thisObject += jsonStringify(object[i]);
+				thisObject += ',';
+			}
+			if (object.length === 0) {
+				thisObject += ']';
+			}
+			else {
+				thisObject = thisObject.slice(0, -1) + ']';
+			}
+			return thisObject;
+		}
+		else if (object.constructor === Object) {
+			thisObject = '{';
+			let objLen = 0;
+			for (let [key, value] of Object.entries(object)) {
+				objLen++;
+				thisObject += '"' + key + '":';
+				thisObject += jsonStringify(value);
+				thisObject += ',';
+			}
+			if (objLen === 0) {
+				thisObject += '}';
+			}
+			else {
+				thisObject = thisObject.slice(0, -1) + '}';
+			}
+			return thisObject;
+		}
+		else {
+			console.log('Missed an Object');
+			console.log(object);
+		}
+	}
+	else {
+		console.log('Missed a case');
+	}
+}
+
+
+let object = JSON.parse(await fs.readFile('bugbjk.json', {encoding: 'utf-8'}));
+
+console.log(JSON.stringify(object));
+console.log(jsonStringify(object));
